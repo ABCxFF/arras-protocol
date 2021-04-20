1d9bbe56dcb8bac4dea65c42cba753c53ea29c31
ABC <79597906+ABCxFF@users.noreply.github.com>
2021-04-20 10:52:11 -0400

    Update stats.js

--- test/stats.js
+++ test/stats.js
@@ -1,1 +1,7 @@
+// @RayAdams and I (ABC) will work on a class that takes in one parameter, the websocket, and retrieves data like total life time, total kill count etc from that.
 
+class StatMonitor {
+  constructor(ws) {
+    this.ws = ws;
+  }
+}
